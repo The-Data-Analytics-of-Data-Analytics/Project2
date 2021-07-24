d4b94c3c73e1962bdad07a232b5377407a73f246
lmodenbach <lmodenbach@gmail.com>
2021-07-24 03:29:36 -0700

    Update logic.js

--- static/logic.js
+++ static/logic.js
@@ -1,9 +1,9 @@
-d3.json("../data/JSON/D3SummaryLangs.json").then(function(engineerTools) {
-  d3.json("../data/JSON/LeafletDatasets/engineerLangs.json").then(function(engineerLangs) {
-    d3.json("../data/JSON/LeafletDatasets/scientistTools.json").then(function(scientistTools) {
-      d3.json("../data/JSON/LeafletDatasets/scientistLangs.json").then(function(scientistLangs) {
-        d3.json("../data/JSON/LeafletDatasets/analystTools.json").then(function(analystTools)  {
-          d3.json("../data/JSON/LeafletDatasets/analystLangs.json").then(function(analystLangs) {
+d3.json("static/engineerTools.json").then(function(engineerTools) {
+  d3.json("static/engineerLangs.json").then(function(engineerLangs) {
+    d3.json("static/scientistTools.json").then(function(scientistTools) {
+      d3.json("static/scientistLangs.json").then(function(scientistLangs) {
+        d3.json("static/analystTools.json").then(function(analystTools)  {
+          d3.json("static/analystLangs.json").then(function(analystLangs) {
             
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -39,18 +39,13 @@ d3.json("../data/JSON/D3SummaryLangs.json").then(function(engineerTools) {
       };
       
       var map = L.map("careerMap", {
-        center: [40.73, -74.0059],
-        zoom: 12,
-        layers: [
-          layers.Engineer,
-          layers.Scientist,
-          layers.Analyst
-        ]
+        center: [37.8044, -122.2712],
+        zoom: 4
       });
 
       streetmap.addTo(map);
-      satellitemap.addTo(map);
       lightmap.addTo(map);
+      satellitemap.addTo(map);
 
       var overlays = {
         "Data Engineer": layers.Engineer,
@@ -60,26 +55,45 @@ d3.json("../data/JSON/D3SummaryLangs.json").then(function(engineerTools) {
      
       var baseMaps = {
         "Street Map": streetmap,
-        "Satellite Map": satellitemap,
-        "Light Map": lightmap
-    };
+        "Light Map": lightmap,
+        "Satellite Map": satellitemap
+      };
 
       L.control.layers(baseMaps, overlays, {collapsed: false}).addTo(map);
 
         var datasets = [engineerTools, engineerLangs, scientistTools, scientistLangs, analystTools, analystLangs];
+        var topTools = [];
+        var topLanguages = [];
 
         datasets.forEach( function (dataset, j) {
         for (var i = 1; i < dataset.length; i++) { 
+          for (var i = 1; i < dataset.length; i++) { 
             if (j < 2)
-              layerIndex = "Engineer";
+              layer = layers.Engineer;
             else if (j < 4)
-              layerIndex = "Scientist";
+              layer = layers.Scientist;
             else
-              layerIndex = "Analyst";
-            var newMarker = L.marker([dataset.lat, dataset.long]); //change reference to data to fit the structure of the JSON
-            newMarker.addTo(layers[layerIndex]);
-            newMarker.bindPopup("<p>Top Tools: " + topTools + "</p><hr><p>Top Languages: " + topLanguages + "</p><hr><p>Average Starting Salary: " + avgStartingSalary + "</p>"); //html table
+              layer = layers.Analyst;
+            
+            dataset[i].lat = +dataset[i].lat;
+            dataset[i].long = +dataset[i].long;
+  
+            if (dataset[i].lat === null || isNaN(dataset[i].lat))
+                continue
+            if (dataset[i].long === null || isNaN(dataset[i].long))
+                continue
+
+            if (topTools.length === 0)
+              topTools.push("None found here");
+  
+            if (topLanguages.length === 0)
+              topLanguages.push("None found here");
+
+            var newMarker = L.marker([dataset[i].lat, dataset[i].long]); //change reference to data to fit the structure of the JSON
+            newMarker.addTo(layer);
+            newMarker.bindPopup("<p>Top Tools: " + topTools + "</p><hr><p>Top Languages: " + topLanguages); // + "</p><hr><p>Average Starting Salary: " + avgStartingSalary + "</p>"); //html table
            
+        }
       }
     });
     
